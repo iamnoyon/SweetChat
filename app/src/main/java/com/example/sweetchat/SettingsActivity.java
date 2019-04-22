82aa2855948d0f5f8b086471d99aa7566ebca3d3
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-04-23 04:54:10 +0600

    Complete Profile Picture Updating Functionality.. :)

--- app/src/main/java/com/example/sweetchat/SettingsActivity.java
+++ app/src/main/java/com/example/sweetchat/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.sweetchat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +23,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -35,6 +45,8 @@ public class SettingsActivity extends AppCompatActivity {
     private DatabaseReference RootRef;
 
     private static final int GalleryPick = 1;
+    private StorageReference UserProfileImageRef;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,7 @@ public class SettingsActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         RootRef = FirebaseDatabase.getInstance().getReference();
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
         InitalizeFields();
 
@@ -61,10 +74,11 @@ public class SettingsActivity extends AppCompatActivity {
         userProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent galleryIntent = new Intent();
-                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, GalleryPick);
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1,1)
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .start(SettingsActivity.this);
             }
         });
     }
@@ -74,6 +88,7 @@ public class SettingsActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.set_user_name);
         userStatus = (EditText) findViewById(R.id.set_profile_status);
         userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
+        loadingBar = new ProgressDialog(this);
     }
 
 
@@ -81,10 +96,65 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK && data!=null){
+            Uri ImageUri = data.getData();
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
 
+            if(resultCode == RESULT_OK){
+                loadingBar.setTitle("Set Profile Image");
+                loadingBar.setMessage("Please Wait, Profile Image is Updating..");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+                Uri resultUri = result.getUri();
+
+                final StorageReference filepath = UserProfileImageRef.child(currentUserID+".jpg");
+
+                filepath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(SettingsActivity.this, "Image Uploaded Successfully...", Toast.LENGTH_SHORT).show();
+                        filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri ProfileImageUrl) {
+                                final String downloadUrl = ProfileImageUrl.toString();
+                                RootRef.child("Users").child(currentUserID).child("image")
+                                        .setValue(downloadUrl)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+                                                    Toast.makeText(SettingsActivity.this, "Image Saved In Database..", Toast.LENGTH_SHORT).show();
+                                                    loadingBar.dismiss();
+                                                }
+                                                else{
+                                                    String message = task.getException().toString();
+                                                    Toast.makeText(SettingsActivity.this, "Error "+message, Toast.LENGTH_SHORT).show();
+                                                    loadingBar.dismiss();
+                                                }
+                                            }
+                                        });
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                String message = e.toString();
+                                Toast.makeText(SettingsActivity.this, "Error "+message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String message = e.toString();
+                        Toast.makeText(SettingsActivity.this, "Error "+message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
     }
-
-
 
     private void UpdateSettings() {
 
@@ -133,6 +203,8 @@ public class SettingsActivity extends AppCompatActivity {
 
                             userName.setText(retrieveUserName);
                             userStatus.setText(retrieveStatus);
+
+                            Picasso.get().load(retrieveProfileImage).into(userProfileImage);
                         }
                         else if((dataSnapshot.exists()) && (dataSnapshot.hasChild("name")))
                         {
