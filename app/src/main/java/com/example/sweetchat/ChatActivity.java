24f930384892ee0bbf33f6bd37309c9d9ce8901f
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-07-06 00:24:45 +0600

    Chat List Item Click   Adds Firebase google-services.json

--- app/src/main/java/com/example/sweetchat/ChatActivity.java
+++ app/src/main/java/com/example/sweetchat/ChatActivity.java
@@ -2,12 +2,21 @@ package com.example.sweetchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
+    private String messageReceiverID, messageReceiverName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messageReceiverID = getIntent().getExtras().get("chat_user_id").toString();
+        messageReceiverName = getIntent().getExtras().get("chat_user_name").toString();
+
+
+        Toast.makeText(this, messageReceiverID, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, messageReceiverName, Toast.LENGTH_SHORT).show();
     }
 }
