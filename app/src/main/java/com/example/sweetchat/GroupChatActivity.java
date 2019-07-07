0d8cd92929b6349b5366845471940fc0a62719f1
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-07-08 01:06:31 +0600

    Custom Message Layout Created  and Designed

--- app/src/main/java/com/example/sweetchat/GroupChatActivity.java
+++ app/src/main/java/com/example/sweetchat/GroupChatActivity.java
@@ -1,12 +1,10 @@
 package com.example.sweetchat;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
@@ -27,118 +25,149 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class GroupChatActivity extends AppCompatActivity {
-
+public class GroupChatActivity extends AppCompatActivity
+{
     private Toolbar mToolbar;
     private ImageButton SendMessageButton;
     private EditText userMessageInput;
     private ScrollView mScrollView;
     private TextView displayTextMessages;
-    private String currentGroupName, currentUserID, currentUserName, currentDate, currentTime;
+
     private FirebaseAuth mAuth;
-    private DatabaseReference UserRef, GroupNameRef, GroupMessageKeyRef;
+    private DatabaseReference UsersRef, GroupNameRef, GroupMessageKeyRef;
+
+    private String currentGroupName, currentUserID, currentUserName, currentDate, currentTime;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
+
+
 
         currentGroupName = getIntent().getExtras().get("groupName").toString();
         Toast.makeText(GroupChatActivity.this, currentGroupName, Toast.LENGTH_SHORT).show();
 
+
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
-        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
         GroupNameRef = FirebaseDatabase.getInstance().getReference().child("Groups").child(currentGroupName);
+
+
 
         InitializeFields();
 
-        getUserInfo();
+
+        GetUserInfo();
+
 
         SendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view)
+            {
                 SaveMessageInfoToDatabase();
+
                 userMessageInput.setText("");
+
                 mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
             }
         });
     }
 
+
+
     @Override
-    protected void onStart() {
+    protected void onStart()
+    {
         super.onStart();
+
         GroupNameRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                if(dataSnapshot.exists()){
+            public void onChildAdded(DataSnapshot dataSnapshot, String s)
+            {
+                if (dataSnapshot.exists())
+                {
                     DisplayMessages(dataSnapshot);
                 }
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                if(dataSnapshot.exists()){
+            public void onChildChanged(DataSnapshot dataSnapshot, String s)
+            {
+                if (dataSnapshot.exists())
+                {
                     DisplayMessages(dataSnapshot);
                 }
             }
 
             @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
 
             }
 
             @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
     }
 
-    private void InitializeFields() {
+
+    private void InitializeFields()
+    {
         mToolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle(currentGroupName);
 
         SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
         userMessageInput = (EditText) findViewById(R.id.input_group_message);
-        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
         displayTextMessages = (TextView) findViewById(R.id.group_chat_text_display);
+        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
     }
 
 
-    private void getUserInfo() {
-        UserRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+
+    private void GetUserInfo()
+    {
+        UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.exists())
+                {
                     currentUserName = dataSnapshot.child("name").getValue().toString();
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
-
     }
 
-    private void SaveMessageInfoToDatabase() {
+
+
+
+    private void SaveMessageInfoToDatabase()
+    {
         String message = userMessageInput.getText().toString();
-        String messageKey = GroupNameRef.push().getKey();
+        String messagekEY = GroupNameRef.push().getKey();
 
-        if(TextUtils.isEmpty(message)){
-            Toast.makeText(this, "Write Message First...", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(message))
+        {
+            Toast.makeText(this, "Please write message first...", Toast.LENGTH_SHORT).show();
         }
-
-        else{
+        else
+        {
             Calendar calForDate = Calendar.getInstance();
             SimpleDateFormat currentDateFormat = new SimpleDateFormat("MMM dd, yyyy");
             currentDate = currentDateFormat.format(calForDate.getTime());
@@ -146,31 +175,38 @@ public class GroupChatActivity extends AppCompatActivity {
             Calendar calForTime = Calendar.getInstance();
             SimpleDateFormat currentTimeFormat = new SimpleDateFormat("hh:mm a");
             currentTime = currentTimeFormat.format(calForTime.getTime());
+
+
             HashMap<String, Object> groupMessageKey = new HashMap<>();
             GroupNameRef.updateChildren(groupMessageKey);
-            GroupMessageKeyRef = GroupNameRef.child(messageKey);
-            HashMap<String, Object> messageInfoMap = new HashMap<>();
-                messageInfoMap.put("name",currentUserName);
-                messageInfoMap.put("message",message);
-                messageInfoMap.put("date",currentDate);
-                messageInfoMap.put("time",currentTime);
-            GroupMessageKeyRef.updateChildren(messageInfoMap);
 
+            GroupMessageKeyRef = GroupNameRef.child(messagekEY);
+
+            HashMap<String, Object> messageInfoMap = new HashMap<>();
+            messageInfoMap.put("name", currentUserName);
+            messageInfoMap.put("message", message);
+            messageInfoMap.put("date", currentDate);
+            messageInfoMap.put("time", currentTime);
+            GroupMessageKeyRef.updateChildren(messageInfoMap);
         }
     }
 
-    private void DisplayMessages(DataSnapshot dataSnapshot) {
+
+
+    private void DisplayMessages(DataSnapshot dataSnapshot)
+    {
         Iterator iterator = dataSnapshot.getChildren().iterator();
-        while (iterator.hasNext()){
+
+        while(iterator.hasNext())
+        {
             String chatDate = (String) ((DataSnapshot)iterator.next()).getValue();
             String chatMessage = (String) ((DataSnapshot)iterator.next()).getValue();
             String chatName = (String) ((DataSnapshot)iterator.next()).getValue();
             String chatTime = (String) ((DataSnapshot)iterator.next()).getValue();
 
-            displayTextMessages.append(chatName + " :\n" + chatMessage + "\n" + chatTime + "    " + chatDate + "\n\n\n");
+            displayTextMessages.append(chatName + " :\n" + chatMessage + "\n" + chatTime + "     " + chatDate + "\n\n\n");
 
             mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
         }
     }
-
 }
--- app/src/main/java/com/example/sweetchat/GroupsFragment.java
+++ app/src/main/java/com/example/sweetchat/GroupsFragment.java
@@ -3,7 +3,6 @@ package com.example.sweetchat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,14 +26,15 @@ import java.util.Set;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GroupsFragment extends Fragment {
-
+public class GroupsFragment extends Fragment
+{
     private View groupFragmentView;
     private ListView list_view;
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_groups = new ArrayList<>();
 
     private DatabaseReference GroupRef;
+
 
 
     public GroupsFragment() {
@@ -44,44 +44,61 @@ public class GroupsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+                             Bundle savedInstanceState)
+    {
         groupFragmentView = inflater.inflate(R.layout.fragment_groups, container, false);
 
-        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
-        
-        InitializeFields();
 
-        RetrieveAndDysplayGroups();
+        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
+
+
+        IntializeFields();
+
+
+        RetrieveAndDisplayGroups();
+
+
 
         list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String currentGroupName = parent.getItemAtPosition(position).toString();
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                String currentGroupName = adapterView.getItemAtPosition(position).toString();
+
                 Intent groupChatIntent = new Intent(getContext(), GroupChatActivity.class);
-                groupChatIntent.putExtra("groupName", currentGroupName);
+                groupChatIntent.putExtra("groupName" , currentGroupName);
                 startActivity(groupChatIntent);
             }
         });
 
+
         return groupFragmentView;
     }
 
-    private void InitializeFields() {
+
+
+    private void IntializeFields()
+    {
         list_view = (ListView) groupFragmentView.findViewById(R.id.list_view);
         arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list_of_groups);
         list_view.setAdapter(arrayAdapter);
     }
 
-    private void RetrieveAndDysplayGroups() {
+
+
+
+    private void RetrieveAndDisplayGroups()
+    {
         GroupRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                Set<String> set = new HashSet<>(0);
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                Set<String> set = new HashSet<>();
                 Iterator iterator = dataSnapshot.getChildren().iterator();
-                while(iterator.hasNext()){
-                    set.add(((DataSnapshot) iterator.next()).getKey());
+
+                while (iterator.hasNext())
+                {
+                    set.add(((DataSnapshot)iterator.next()).getKey());
                 }
 
                 list_of_groups.clear();
@@ -90,7 +107,7 @@ public class GroupsFragment extends Fragment {
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
